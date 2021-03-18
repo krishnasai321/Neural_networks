3f23bdf396edd0d4049189247e5869550a94919b
Sai krishna <saikrishna321@gmail.com>
2021-03-18 18:37:07 +0530

    Updated

--- Convolution neural networks (CNN)/cnn_practice_code.py
+++ Convolution neural networks (CNN)/cnn_practice_code.py
@@ -5,6 +5,7 @@ Created on Thu Mar 18 18:10:54 2021
 @author: kqll413
 """
 
+#Import data, libraries
 import keras
 from keras.datasets import fashion_mnist 
 from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
@@ -13,21 +14,24 @@ from keras.utils import to_categorical
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Loading train and test data from fashion_mnist
 (train_X,train_Y), (test_X,test_Y) = fashion_mnist.load_data()
 
+#Reshaping for all the grayscale datasets
 train_X = train_X.reshape(-1, 28,28, 1)
 test_X = test_X.reshape(-1, 28,28, 1)
 
-train_X.shape
-
+#Changing to decimals and normalizing 0-1
 train_X = train_X.astype('float32')
 test_X = test_X.astype('float32')
 train_X = train_X / 255
 test_X = test_X / 255
 
+#Encoding dependent variable (categorical crossentropy loss)
 train_Y_one_hot = to_categorical(train_Y)
 test_Y_one_hot = to_categorical(test_Y)
 
+#Setting up NN layers
 model = Sequential()
 model.add(Conv2D(64, (3,3), input_shape=(28, 28, 1)))
 model.add(Activation('relu'))
@@ -39,16 +43,17 @@ model.add(Flatten())
 model.add(Dense(64))
 model.add(Dense(10))
 model.add(Activation('softmax'))
+
+#Compiling NN
 model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
 
+#Fitting the models
 model.fit(train_X, train_Y_one_hot, batch_size=64, epochs=10)
 
-test_loss, test_acc = model.evaluate(test_X, test_Y_one_hot)
-print('Test loss', test_loss)
-print('Test accuracy', test_acc)
-
+#Prediction
 predictions = model.predict(test_X)
 print(np.argmax(np.round(predictions[0])))
 
+#Plot first image
 plt.imshow(test_X[0].reshape(28, 28), cmap = plt.cm.binary)
 plt.show()
