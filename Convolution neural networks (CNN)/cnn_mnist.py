7aa5a423dd4b32c6128f5cac4e2220872ee850b8
Sai krishna <saikrishna321@gmail.com>
2021-03-20 19:58:32 +0530

    Updated

---
+++ Convolution neural networks (CNN)/cnn_mnist.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 19:36:04 2021
+
+@author: Sai Krishna
+"""
+
+#Import data, libraries
+import keras
+from keras.datasets import mnist
+from keras.layers import Dense, Activation, MaxPooling2D, Conv2D, Flatten
+from keras.models import Sequential
+from keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Loading train and test data from fashion_mnist
+(train_X,train_Y), (test_X,test_Y) = mnist.load_data()
+
+#Reshaping for all the grayscale datasets
+train_X = train_X.reshape(-1, 28,28, 1)
+test_X = test_X.reshape(-1, 28,28, 1)
+
+#Changing to decimals and normalizing 0-1
+train_X = train_X.astype('float32')
+test_X = test_X.astype('float32')
+train_X = train_X / 255
+test_X = test_X / 255
+
+#Encoding dependent variable (categorical crossentropy loss)
+train_Y_one_hot = to_categorical(train_Y)
+test_Y_one_hot = to_categorical(test_Y)
+
+#Setting up NN layers
+model = Sequential()
+model.add(Conv2D(64, (3,3), input_shape=(28, 28, 1)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(64, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+#Compiling NN
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+#Fitting the models
+model.fit(train_X, train_Y_one_hot, batch_size=64, epochs=10)
+
+#Prediction
+predictions = model.predict(test_X)
+print(np.argmax(np.round(predictions[0])))
+
+#Plot first image
+plt.imshow(test_X[0].reshape(28, 28), cmap = plt.cm.binary)
+plt.show()
+
+
